6db701b60c1d81eb2f51bfb630f961209e3fab33
Izmaelhakki <100159172+Izmaelhakki@users.noreply.github.com>
2023-01-23 15:29:38 +0300

    Auth working

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets,", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets,", express.static(path.join(__dirname, "/server/public/assets")));
 
 //File Storage
 const storage = multer.diskStorage({
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,23 +1,22 @@
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const verifyToken = async (req, res, next) => {
   try {
-    //Token headerdan değişkene atanır
     let token = req.header("Authorization");
-    //Kontrol edilir
+
     if (!token) {
-      res.status(403).send("Access Denied");
+      return res.status(403).send("Access Denied");
     }
-    //Gelen token Bearer: ile başlamakta temizlemek için aşağıdaki işlem uygulanır
+
     if (token.startsWith("Bearer ")) {
-      token = token.slice(7, tokens.length).trimLeft();
+      token = token.slice(7, token.length).trimLeft();
     }
-    //jwt ile token kontrolü yapılır,usera verified verilir ve diğer işleme geçilir,
+
     const verified = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = verified;
     next();
-    
   } catch (err) {
-    res.status(500).json({ error: err.msg });
+    res.status(500).json({ error: err.message });
   }
 };
